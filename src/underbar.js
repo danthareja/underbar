551fb37a8ad52ab6dab5cd42d7d6f7897caafa16
Hack Reactor Students <danthareja@gmail.com>
2014-07-22 20:05:35 -0700

    started extend

--- src/underbar.js
+++ src/underbar.js
@@ -248,6 +248,11 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.apply(arguments,1)
+
+    _.each(args, function (value, key) {
+      obj[key] = value;
+    })
   };
 
   // Like extend, but doesn't ever overwrite a key that already
